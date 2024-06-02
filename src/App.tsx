00af405a57b124bc9d97d7d136b708ff905a466b
Nodirbek Azizov <azizovv.nodirbek@gmail.com>
2024-06-02 17:43:41 +0500

    Created an application

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "./shared/sidebar/sidebar"
+
+function App() {
+  
+
+  return (
+    <>
+      <Sidebar>
+        
+      </Sidebar>
+    </>
+  )
+}
+
+export default App
---
+++ src/shared/components/sidebar/sidebar.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+        <div>
+            <div></div>
+            <Outlet></Outlet>
+        </div>
+  )
+}
+
+export default Sidebar
